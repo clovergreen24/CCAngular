501de1d46bd6ca0459ab81e2e92241ecd5c8f685
Camilo Cobeaga <cobeagac@gmail.com>
2023-12-21 06:57:26 -0300

    se instalo flowbite

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,7 +3,6 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { LoginComponent } from './login/login.component';
 import { ReactiveFormsModule, FormsModule } from '@angular/forms';
 import { UsuarioComponent } from './usuario/usuario.component';
 
@@ -12,7 +11,6 @@ import { HttpClientModule } from '@angular/common/http';
 @NgModule({
   declarations: [
     AppComponent,
-    LoginComponent,
     UsuarioComponent
   ],
   imports: [
