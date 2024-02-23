647ca6e5b892506ee84169f699e9a0604a9943c2
clovergreen24 <cobeagac@gmail.com>
2024-02-23 00:29:36 -0300

    avance con creacion de gasto

--- src/app/service/grupo.service.ts
+++ src/app/service/grupo.service.ts
@@ -4,6 +4,8 @@ import { Observable } from 'rxjs';
 import { Grupo } from '../model/grupo.interface';
 import { Usuario } from '../model/usuario.interface';
 import { UsuarioService } from './usuario.service';
+import { Gasto } from '../model/gasto.interface';
+import { Categoria } from '../model/categoria.interface';
 
 @Injectable({
   providedIn: 'root'
@@ -20,11 +22,25 @@ export class GrupoService {
     return grupos
   }
 
-  public getGrupo(id: string | null){
+  getGrupo(id: string | null){
     return this.http.get<Grupo>(this.urlApi + '/' + id)
   }
 
   getIntegrantes(id: string | null):Observable<Usuario[]>{
     return this.http.get<Usuario[]>(this.urlApi + '/' + id + '/miembros')
   }
+
+  getGastos(id: string | null):Observable<Gasto[]>{
+    return this.http.get<Gasto[]>(this.urlApi + '/' + id + '/gastos') 
+  }
+
+  getCategorias(){
+    return this.http.get<Categoria[]>('http://localhost:8080/jwt/categoria')
+  }
+
+  createGasto(id: string | null,gasto: Gasto){
+    console.log("grupo service")
+    return this.http.post<Grupo>(this.urlApi + '/' + id + '/agregarGasto', gasto)
+  }
+  
 }
